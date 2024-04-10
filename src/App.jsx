019f18e43618d78ae0e74fadbd79d392c25f8a48
Bikal <bikaladhikari5@gmail.com>
2024-04-10 13:49:45 +1000

    Experience section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
+import Experience from "./components/Experience/Experience";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
     </div>
   );
 };
