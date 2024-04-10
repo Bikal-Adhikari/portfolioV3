87b0039f0bf1e6772c58450109b09abf2b48059c
Bikal <bikaladhikari5@gmail.com>
2024-04-10 17:52:48 +1000

    dark mode toggling working

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import { ThemeProvider } from "./Context.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
