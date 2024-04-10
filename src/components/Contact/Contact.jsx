610e9cd2b0b8ae6b618f9c48125d423619c30a0b
Bikal <bikaladhikari5@gmail.com>
2024-04-10 17:00:25 +1000

    Contact me page completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const form = useRef();
+
+  const [done, setDone] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_tenqrl4", "template_9z6xm78", form.current, {
+        publicKey: "MfgJSv6Rr59RVJSG6",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setDone(true);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Get in Touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+      <div className="c-right">
+        <form action="" ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+          />
+          <textarea
+            name="message"
+            className="user"
+            placeholder="Message"
+          ></textarea>
+          <input type="submit" value="Send" className="button" />
+          <span>{done && "Thanks for contacting me!"}</span>
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
