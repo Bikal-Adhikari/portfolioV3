54ced54bf35c3798ea7a63e8f143fb6a7446c97c
Bikal <bikaladhikari5@gmail.com>
2024-04-10 10:39:10 +1000

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Bikal</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyle: "none" }}>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button n-button">Contact me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
